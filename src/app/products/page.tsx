fb0cd7cccc3635785e806bf39ea26274cbfd7c14
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-04-26 22:55:43 +0600

    feat: implement Product component with layout and Add Product button

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function Product() {
-  return <div>Products page</div>;
+  return (
+    <div className="flex flex-col mt-2">
+      <div className="justify-end">
+        <Button className="bg-blue-500 hover:bg-blue-600 cursor-pointer text-white font-bold py-2 px-4 rounded">
+          Add Product
+        </Button>
+      </div>
+      <div></div>
+    </div>
+  );
 }
