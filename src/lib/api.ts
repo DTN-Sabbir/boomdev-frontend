e7ccd1d46fd0068e54c3a54f7dfcd7857d0751e4
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-04-27 11:45:13 +0600

    fix: standardize endpoint key to uppercase for consistency

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
 const localBaseUrl = process.env.NEXT_PUBLIC_LOCAL_BASE_URL;
 export const Endpoints = {
-  Products: `${localBaseUrl}api/v1/products/`,
+  PRODUCTS: `${localBaseUrl}api/v1/products/`,
 };
